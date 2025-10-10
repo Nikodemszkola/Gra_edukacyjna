8527a2190e8df397222f0c5a95b6f4c221da6bad
niko <niko@gmail.com>
2025-10-10 11:15:01 +0200

    dawdaw

--- skrypt.js
+++ skrypt.js
@@ -1,19 +1,20 @@
-function slowka_definicje() {
+function slowka_definicje(e) {
     const ile = parseInt(document.getElementById("ile").value);
     const druk = document.getElementById("druk");
     const druk2 = document.getElementById("druk2");
 
     druk.innerHTML = "";
     druk2.innerHTML = "";
-
+    if (e.key === "Enter"){
     if (!isNaN(ile) && ile >= 1 && ile <= 10) {
         for (let i = 1; i <= ile; i++) {
+            
             druk.innerHTML += `<p>Słówko nr ${i}: <input type="text"></p>`;
             druk2.innerHTML += `<p>Definicja nr ${i}: <input type="text"></p>`;
         }
     } else {
         druk.innerHTML = "<p>Wpisz liczbę od 1 do 10.</p>";
-    }
+    }}
 }
 
 
